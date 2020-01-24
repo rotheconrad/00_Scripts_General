0293ba345fe481541c4149abacd565c9c08f35c7
Roth Conrad <rotheconrad@gatech.edu>
2020-01-24 12:39:12 -0500

    Add scripts

--- Quick_Match_tsv_columns.py
+++ Quick_Match_tsv_columns.py
@@ -1,49 +1,101 @@
-# python
+#!/usr/bin/env python
 
-## for two tsv files
-## Select lines from file2 that a match a column in file1
+''' Quick Match Columns of 2 tsv files.
 
-import sys
+For two tsv files:
+Select lines from file 2 that match a column in file 1.
+Print matching lines from file 2 to output file.
 
-if len(sys.argv[1:]) != 5:
-    print(
-        '\n\nFor two *.tsv files with 1st line header:\n'
-        'This script selects lines in file 2 that match a column in file 1\n'
-        'and writes them to a new file.\n\n'
-        'This script takes 5 input parameters in order:\n'
-        '\tp1: name of file 1 to select column to find in file 2\n'
-        '\tp2: column number to select unique id from (starting at 0)\n'
-        '\tp3: name of file 2 to select lines matching column in file 1\n'
-        '\tp4: ncolumn number to select unique id for file 2\n'
-        '\tp5: name of the output file\n'
-        '\n\nExample:\npython scriptname.py p1 p2 p3 p4 p5\n\n'
+Takes 5 positional arguments.
+file1, column number, file2, column number, output file
+
+* Column numbers start at 0 (ie: python is 0 indexed)
+
+-------------------------------------------
+Author :: Roth Conrad
+Email :: rotheconrad@gatech.edu
+GitHub :: https://github.com/rotheconrad
+Date Created :: June 21st, 2019
+License :: GNU GPLv3
+Copyright 2019 Roth Conrad
+All rights reserved
+-------------------------------------------
+'''
+
+import argparse
+
+def match_columns(f1, c1, f2, c2, o):
+
+    # dict of unique column ids from file 1 to look for in file 2
+    match = {}
+
+    # Retrieve unique ids from f1, c1 and populate match dict
+    with open(f1, 'r') as f:
+        header = f.readline()
+        for line in f:
+            match_id = line.rstrip().split('\t')[c1]
+            match[match_id] = ''
+
+    # Look for match dict keys in file 2 and write new file
+    with open(file2, 'r') as f, open(outfile, 'w') as o:
+        header = f.readline()
+        o.write(header)
+        for line in f:
+            X = line.rstrip().split('\t')[c2]
+            if X in match:
+                o.write(line)
+
+def main():
+
+        # Configure Argument Parser
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
         )
-    sys.exit('Please provide all 5 input parameters\n\n')
+    parser.add_argument(
+        '-f1', '--input_file_1',
+        help='Please specify the file to retrieve unique IDs from!',
+        metavar=':',
+        type=str,
+        required=True
+        )
+    parser.add_argument(
+        '-c1', '--select_column_1',
+        help='Please specify the column number to select (0 indexed)!',
+        metavar=':',
+        type=int,
+        required=True
+        )
+    parser.add_argument(
+        '-f2', '--input_file_2',
+        help='Please specify the file to match unique IDs to!',
+        metavar=':',
+        type=str,
+        required=True
+        )
+    parser.add_argument(
+        '-c2', '--select_column_2',
+        help='Please specify the column number to match (0 indexed)!',
+        metavar=':',
+        type=int,
+        required=True
+        )
+    parser.add_argument(
+        '-o', '--output_file_name',
+        help='What do you want to name the output file?',
+        metavar='',
+        type=str,
+        required=True
+        )
+    args=vars(parser.parse_args())
 
-# first file name
-file1 = sys.argv[1]
-# column to select from first file (count columns starting at 0)
-colnum1 = int(sys.argv[2])
-# second file name
-file2 = sys.argv[3]
-# column to select from second file (count columns starting at 0)
-colnum2 = int(sys.argv[4])
-# Output file name
-outfile = sys.argv[5]
+    match_columns(
+                    args['input_file_1'],
+                    args['select_column_1'],
+                    args['input_file_2'],
+                    args['select_column_2'],
+                    args['output_file_name']
+                    )
 
-# dict of unique column ids from file 1 to look for in file 2
-match = {}
-
-with open(file1, 'r') as f1:
-    header = f1.readline()
-    for line in f1:
-        match_id = line.rstrip().split('\t')[colnum1]
-        match[match_id] = ''
-
-with open(file2, 'r') as f2, open(outfile, 'w') as o:
-    header = f2.readline()
-    o.write(header)
-    for line in f2:
-        X = line.rstrip().split('\t')[colnum2]
-        if X in match:
-            o.write(line)
+if __name__ == "__main__":
+    main()
